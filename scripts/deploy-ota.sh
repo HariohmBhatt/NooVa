71a011327650364472412580472e5514ffd23275
Hariohm Bhatt <bhatthariohm2004@gmail.com>
2026-08-16 15:11:22 +0000

    chore: clear branch for new canvas

--- scripts/deploy-ota.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-
-: "${NOVA_OTA_HOST:?Set NOVA_OTA_HOST to the device IP address}"
-: "${NOVA_OTA_PASSWORD:?Set NOVA_OTA_PASSWORD to the SSH password}"
-
-pio run -e nova-ota --target upload
--- scripts/ui-preview.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-set -eu
-
-repo_root=$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)
-preview_port=${NOVA_PREVIEW_PORT:-4173}
-
-cd "$repo_root/tools/ui-preview"
-exec python3 -m http.server "$preview_port" --bind 127.0.0.1
